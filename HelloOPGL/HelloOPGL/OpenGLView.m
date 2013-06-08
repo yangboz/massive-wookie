1d658a870aa2a85731a227e2e549da03b074d3c1
Knight.zhou <YoungWelle@gmail.com>
2013-06-08 22:46:09 +0800

    CHANGED:added the vertex data structure and its data

--- HelloOPGL/HelloOPGL/OpenGLView.m
+++ HelloOPGL/HelloOPGL/OpenGLView.m
@@ -9,6 +9,37 @@
 #import "OpenGLView.h"
 
 @implementation OpenGLView
+//Vertex data
+typedef struct
+{
+    float Position[3];
+    float Color[4];
+}Vertex;
+
+const Vertex Vertices[] = {
+    {{1,-1,0},{1,0,0,1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0,1,2,
+    2,3,0
+};
+
+-(void)setupVBOs
+{
+    GLuint vertextBuffer;
+    glGenBuffers(1, &vertextBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertextBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Indices, GL_STATIC_DRAW);
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
 //Vectex and Fragment
 -(GLuint)compileShader:(NSString *)shaderName withType:(GLenum)shaderType
 {
@@ -81,6 +112,7 @@
         [self setupContext];
         [self setupRenderBuffer];
         [self setupFrameBuffer];
+        [self setupVBOs];
         [self render];
         //
         [self compileShader];
@@ -139,6 +171,16 @@
 {
     glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+//    [_context presentRenderbuffer:GL_RENDERBUFFER];
+    //1
+//    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    glViewport(0, 0, 320, 480);
+    //2
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)(sizeof(float)*3));
+    //3
+    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
+    
     [_context presentRenderbuffer:GL_RENDERBUFFER];
 }
 /*
